05fb3ad4f105e1b3c9f80bddbcee3655b6d2d0ca
thanhngancao <spaceback1999@gmail.com>
2020-06-16 20:53:17 +0700

    first commit

---
+++ src/compoment/view/Home.js
@@ -0,0 +1,175 @@
+import React,{useState, useEffect,useContext} from 'react'
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+    const [data, setData] = useState([])
+    const {state, dispatch} = useContext(UserContext)
+    useEffect(()=>{
+        fetch('/newsfeed',{
+            headers:{
+                "Authorization": "" + localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result => {
+            console.log(result)
+            setData(result.posts)
+        })
+    },[])
+
+    // const likePost = (id) => {
+    //     fetch('/like', {
+    //         method: "put",
+    //         headers:{
+    //             "Content-Type":"application/json", 
+    //             "Authorization": "" + localStorage.getItem("jwt")
+    //         },
+    //         body:JSON.stringify({
+    //             postId:id
+    //         })
+    //     }).then(res => res.json())
+    //     .then(result => {
+    //         //console.log(result)
+    //         const newData = data.map(item => {
+    //             if(item._id == result._id){
+    //                 return result
+    //             }else{
+    //                 return item
+    //             }
+    //         })
+    //         setData(newData)
+    //     }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }
+
+    // const unlikePost = (id) => {
+    //     fetch('/unlike', {
+    //         method: "put",
+    //         headers:{
+    //             "Content-Type":"application/json", 
+    //             "Authorization": "" + localStorage.getItem("jwt")
+    //         },
+    //         body:JSON.stringify({
+    //             postId:id
+    //         })
+    //     }).then(res => res.json())
+    //     .then(result => {
+    //        // console.log(result)
+    //         const newData = data.map(item => {
+    //             if(item._id == result._id){
+    //                 return result
+    //             }else{
+    //                 return item
+    //             }
+    //         })
+    //         setData(newData)
+    //     }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }
+
+    const makeComment = (text, postId) =>{
+        fetch('/comment', {
+            method:"put",
+            headers:{
+                "Content-Type":"application/json", 
+                "Authorization": "" + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res => res.json())
+        .then(result => {
+            console.log(result)
+            const newData = data.map(item => {
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const deletePost = (postid) => {
+        fetch(`/deletepost/${postid}`,{
+            method:"delete",
+            headers:{
+                "Authorization": "" + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+        .then(result => {
+            console.log(result)
+            const newdata = data.filter(item => {
+                return item._id !== result._id
+            })
+            setData(newdata)
+        })
+    }
+    return(
+        <div className="home">
+            {
+                data.map(item => {
+                    return(
+                        <div className="card home-card" key={item._id}>
+                            <h6 style={{margin:"10px", fontWeight:"bold", fontSize:"20px"}}><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile/"}>
+                                    {item.postedBy.name}
+                                </Link> 
+                                {item.postedBy._id == state._id && 
+                                 <i className="material-icons" style={{float:"right"}}
+                                    onClick={() => deletePost(item._id)}>
+                                    delete
+                                 </i>
+                                }
+                                
+                            </h6>
+                        <div className="card-image">
+                            <img src={item.picture}/>
+                        </div>
+                        <div className="card-content">
+                            {/* <i className="material-icons" style={{color:"red"}}>favorite</i> */}
+                            {/* {item.likes.includes(state._id)
+                               ? 
+                               <i className="material-icons " style={{color:"red"}}
+                                   onClick={()=>{unlikePost(item._id)}}>favorite</i>
+                               : 
+                               <i className="material-icons" style={{color:"red"}}
+                                   onClick={()=>{likePost(item._id)}}>favorite_border</i>
+                            }
+                            <h6>{item.likes.length} likes</h6> */}
+                            <h6>{item.title}</h6>
+                            <p>{item.body}</p>
+                            {
+                                item.comments.map(record => {
+                                    return(
+                                    <h6 key={record._id}>
+                                        <span style={{fontWeight:"bold"}}>
+                                        {record.postedBy.name}
+                                        </span><span>   </span>
+                                        {record.text}
+                                    </h6>
+                                    )
+                                })
+                            }
+                            <form onSubmit={(e) => {
+                                e.preventDefault()
+                                makeComment(e.target[0].value, item._id)
+                                //makeComment(e.target[0].value, item._id)
+                            }}>
+                                <input type="text" placeholder="comment"/>
+                            </form>
+                        </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/compoment/view/Profile.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState, useContext} from 'react'
+import {UserContext} from '../../App'
+
+const Profile = () => {
+    const [mypic, setMypic] = useState([])
+    const {state, dispatch} = useContext(UserContext)
+    useEffect(()=>{
+        fetch('/mynewsfeed', {
+            headers:{
+                "Authorization": "" + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+        .then(result => {
+            setMypic(result.mynewsfeed)
+        })
+    },[])
+    return(
+        <div style={{maxWidth:"700px", margin:"0px auto", color: "white"}}>
+            <div style ={{
+                display:"flex",
+                justifyContent: "space-around",
+                margin: "38px 0px"
+            }}>
+                <div>
+                    <img style={{width:"160px", height:"160px", borderRadius:"80px"}}
+                    src={state?state.pic:"loading"}/>
+                </div>
+                <div>
+                    <h4>
+                        {state?state.name:"loading"}
+                    </h4>
+                    <div style={{display:"flex", justifyContent:"space-between", width:"100%", marginLeft:"20px", color: "white"}}>
+                        <h6>{mypic.length} post</h6>
+                    </div>
+
+                </div>
+            </div>
+            <div className="gallery">
+                {
+                    mypic.map(item => {
+                        return(
+                            <img key={item._id} className="item" src={item.picture} alt={item.title}/>
+                        )
+                    })
+                }
+                
+                
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
